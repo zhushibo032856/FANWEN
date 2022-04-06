342ed93b5201a0ee371f3ce9dfa755be2d5391bf
fanews <fanews@fanewsdeMacBook-Pro.local>
2022-04-06 09:29:02 +0800

    commit

---
+++ FANEWS/ViewControllers/个人中心/ViewController/编辑资料/ViewController/ZZEditInfoVC.m
@@ -0,0 +1,32 @@
+//
+//  ZZEditInfoVC.m
+//  FANEWS
+//
+//  Created by fanews on 2022/4/2.
+//  Copyright © 2022 Fanews. All rights reserved.
+//
+
+#import "ZZEditInfoVC.h"
+
+@interface ZZEditInfoVC ()
+
+@end
+
+@implementation ZZEditInfoVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
